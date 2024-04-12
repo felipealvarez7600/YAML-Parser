af4b419eb4cb234b2b98e7da537dffe691217079
Eduardo Ramos <80893262+Udemos@users.noreply.github.com>
2024-04-12 16:15:24 +0100

    YamlDate updated

--- yamlify/src/main/kotlin/pt/isel/YamlAny.kt
+++ yamlify/src/main/kotlin/pt/isel/YamlAny.kt
@@ -1,6 +1,6 @@
 package pt.isel
 
 import java.time.LocalDate
-class YamlDate {
+class YamlAny {
     fun convert(input: String): LocalDate? = LocalDate.parse(input.drop(1).dropLast(1).split(", ").joinToString("-") { it.split("=")[1] })
 }
