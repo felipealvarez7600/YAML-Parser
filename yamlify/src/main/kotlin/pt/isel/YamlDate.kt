74c67f9ca52d1137968c9856d2a95454d01649d3
Eduardo Ramos <80893262+Udemos@users.noreply.github.com>
2024-04-12 15:17:02 +0100

    YamlDate updated

--- yamlify/src/main/kotlin/pt/isel/YamlDate.kt
+++ yamlify/src/main/kotlin/pt/isel/YamlDate.kt
@@ -2,10 +2,5 @@ package pt.isel
 
 import java.time.LocalDate
 class YamlDate {
-    fun convert(input: String): LocalDate? {
-        val datePattern = "\\{year=(\\d{4}), month=(\\d{2}), day=(\\d{2})}".toRegex()
-        val matchResult = datePattern.find(input) ?: throw IllegalArgumentException("Invalid date format: $input")
-        val (year, month, day) = matchResult.destructured
-        return LocalDate.of(year.toInt(), month.toInt(), day.toInt())
-    }
+    fun convert(input: String): LocalDate? = LocalDate.parse(input.drop(1).dropLast(1).split(", ").joinToString("-") { it.split("=")[1] })
 }
