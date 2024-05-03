b54b6ea61e72e61bfe3c6948eb46b70e5e011da0
felipe-alvarez <113711798+felipe-alvarez@users.noreply.github.com>
2024-05-03 15:13:49 +0100

    Changed several things in several files and the 1.2 is completed with cojen

---
+++ yamlify/src/test/kotlin/pt/isel/test/StudentYaml.kt
@@ -0,0 +1,15 @@
+package pt.isel.test
+
+import pt.isel.Details
+import pt.isel.YamlAny
+import pt.isel.annotations.YamlArg
+import pt.isel.annotations.YamlConvert
+import java.time.LocalDate
+
+class StudentYaml @JvmOverloads constructor (
+    val name: String,
+    val nr: Int,
+    @YamlArg("city of birth")
+    val from: String,
+    val address: Address? = null
+)
